523921bbed83749e2bbe8b806898cdc58ac6abcd
dafnesabrina1 <dafnesabrina1@gmail.com>
2019-04-22 18:08:59 -0500

    Twilio1.0

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import datetime
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = Flask(__name__)
 CORS(app)
@@ -17,6 +18,13 @@ from models import Usuarios
 @app.route("/")
 def hello():
     return "puto el que lo lea exepto Dafne"
+
+@app.route("/sms", methods=['POST'])
+def sms_reply():
+    msg = request.form.get('Body')
+    resp = MessagingResponse()
+    resp.message("HERE IS YOUR MESSAGE {}".form(msg))
+    return str(resp)
 
 
 if __name__ == '__main__':

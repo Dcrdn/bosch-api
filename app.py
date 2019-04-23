351c5fd0548754da4e798148eeab4e9358bf6ccf
dafnesabrina1 <dafnesabrina1@gmail.com>
2019-04-22 19:51:50 -0500

    initial commit

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from twilio.twiml.messaging_response import MessagingResponse
-from twilio.rest import Client
 
 app = Flask(__name__)
 CORS(app)
@@ -14,29 +13,18 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-account_sid = 'AC01310a6100555a897c5e4cf36f4bc601'
-auth_token = '5be98f5de25583f76a5e1354f6bd442d'
-client = Client(account_sid, auth_token)
-
 from models import Usuarios
 
 @app.route("/")
 def hello():
     return "puto el que lo lea exepto Dafne, hi"
 
-def send_message(to, body):
-   # Initialize client
-   client = Client(account_sid, auth_token)
-   client.messages.create(
-       body=body,
-       from_=os.getenv('+1 415 523 8886'),
-       to=to
-   )
 @app.route("/sms", methods=['POST'])
 def sms_reply():
-    msg = request.values.get('Body')
-    send_message(to=request.values.get('From'), body="You sent {}".format(msg))
-    return ('', 204)
+    msg = request.form.get('Body')
+    resp = MessagingResponse()
+    resp.message("*HERE IS YOUR MESSAGE*: {}".format(msg))
+    return str(resp)
 
 
 if __name__ == '__main__':

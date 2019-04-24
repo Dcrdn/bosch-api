7540447c1f0c2ee1c54630b7d820d65c8fca346f
dafnesabrina1 <dafnesabrina1@gmail.com>
2019-04-23 23:06:47 -0500

    initial commit

--- app.py
+++ app.py
@@ -13,9 +13,6 @@ CORS(app)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-account_sid = 'AC01310a6100555a897c5e4cf36f4bc601'
-auth_token = '5be98f5de25583f76a5e1354f6bd442d'
-client = Client(account_sid, auth_token)
 
 from models import Usuarios
 
@@ -28,10 +25,6 @@ def sms_reply():
     msg = request.form.get('Body')
     resp = MessagingResponse()
     resp.message("*HERE IS YOUR MESSAGE*: {}".format(msg))
-    message = client.messages.create(
-                              from_='whatsapp:+5213314585897',
-                              body='{}'.format(msg),
-                              to='whatsapp:+5213332005486'
                           )
     return str(message.sid)
 

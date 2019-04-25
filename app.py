ac8b7c42665abef76c79869cdbbdf31d66bcfb45
dafnesabrina1 <dafnesabrina1@gmail.com>
2019-04-24 22:44:37 -0500

    Image

--- app.py
+++ app.py
@@ -214,8 +214,8 @@ def messenger_reply():
         print(request.form.get('From'))
         client.messages.create(
             body="Great. Here you have the bank details",
-            from_=request.form.get('To'),
-            to=request.form.get('From'),
+            from_=request.form.get('From'),
+            to=request.form.get('To'),
             media_url='https://frasesparami.com/wp-content/uploads/2017/06/IMAGENES-DE-RISA-CON-FRASES.jpg'
         )
         toSend="Please provide us an address and name to send the product when your payment is accepted"
